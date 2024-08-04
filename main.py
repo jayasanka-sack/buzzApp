a39d3e195102924e83062cb33c6348ce313a8fad
Dineth Janitha <cryptowrokdjk@gmail.com>
2024-08-04 18:42:28 +0530

    Your commit message here

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, send_from_directory
 from flask_socketio import SocketIO
+import html
 
 app = Flask(__name__, static_folder='frontend')
 socketio = SocketIO(app, cors_allowed_origins="*")
@@ -12,10 +13,20 @@ def index():
 def static_files(path):
     return send_from_directory('frontend', path)
 
+
+def sanitize_input(input):
+    sanitize_str = html.escape(input)
+    return sanitize_str
+
+
 @socketio.on('send_message')
 def handle_send_message(data):
-    print(data)
-    socketio.emit("new_message", data)
+    print("Sanitized data is " + sanitize_input(data["message"]))
+    data["message"] = sanitize_input(data["message"]) 
+    socketio.emit("new_message",data)
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=6003)
+
+
+#<button onclick=alert("hello world")>Hello</button>
